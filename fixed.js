bdac7d48690257896d6f2a1fd3dde9e5f099bbb5
MNITD <bogdanleblank@ukr.net>
2017-01-23 01:41:17 +0200

    minor changed, replaced bg-img to another extention

--- fixed.js
+++ fixed.js
@@ -13,13 +13,4 @@
     for (var i = 0; i < fixedArray.length; i++) {
         fixedArray[i].parentElement.onscroll = getScrollHandler(fixedArray[i]);
     }
-
-    //    window.onscroll = function () {
-    //
-    //        var scrolled = window.pageYOffset || document.documentElement.scrollTop,
-    //            header = document.getElementsByTagName("header")[0];
-    //        header.style.top = scrolled + "px";
-    //                    if (scrolled >= bgHeight * 0.61) {
-    //                        document.getElementById("playMarketBtn").className = "btn btn-scroll-outline";
-    //                    };
 }());
